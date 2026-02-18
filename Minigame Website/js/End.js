522ad1b8d2a2c2d8fd0f1ce4f404ba098f166350
Karim Obegi <karimobegi2007@gmail.com>
2026-02-18 19:05:00 +0000

    Delete Minigame Website directory

--- Minigame Website/js/End.js
+++
@@ -1,8 +0,0 @@
-let currentVariable1 = localStorage.getItem("variable1");
-let currentVariable2 = localStorage.getItem("variable2");
-if (currentVariable1 < currentVariable2) {
-  document.querySelector("body").style.backgroundColor = "#ff0000";
-}
-localStorage.setItem("royalRumbleStarted", "false");
-localStorage.setItem("variable1", "0");
-localStorage.setItem("variable1", "0");
